7a6e35c69d62823876fa3c4dbf0770f2cda780a5
Mike Larah <mike.larah@endjin.com>
2022-12-10 20:53:05 +0000

    Day 08 and 09

--- AdventOfCode.Solutions/Year2022/Day08/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day08/Solution.cs
@@ -2,15 +2,135 @@ namespace AdventOfCode.Solutions.Year2022.Day08;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(08, 2022, "") { }
+    public Solution() : base(08, 2022, "Treetop Tree House") { }
+
+    public static bool IsVisible(int[,] grid, int row, int col)
+    {
+        // On edges
+        if (row == 0 || col == 0) return true;
+        if (row == grid.GetLength(0) - 1) return true;
+        if (col == grid.GetLength(1) - 1) return true;
+
+        int height = grid[row,col];
+
+        // Visible from left
+        bool visibleFromLeft = true;
+        for (int c = 0; c < col; c++)
+        {
+            if (grid[row,c] >= height) { visibleFromLeft = false; break; }
+        }
+
+        // Visible from right
+        bool visibleFromRight = true;
+        for (int c = col + 1; c < grid.GetLength(1); c++)
+        {
+            if (grid[row,c] >= height) { visibleFromRight = false; break; }
+        }
+
+        // Visible from top
+        bool visibleFromTop = true;
+        for (int r = 0; r < row; r++)
+        {
+            if (grid[r,col] >= height) { visibleFromTop = false; break; }
+        }
+
+        // Visible from bottom
+        bool visibleFromBottom= true;
+        for (int r = row + 1; r < grid.GetLength(0); r++)
+        {
+            if (grid[r,col] >= height) { visibleFromBottom = false; break; }
+        }
+
+        return visibleFromLeft || visibleFromRight || visibleFromTop || visibleFromBottom;
+    }
+
+    public static int ScenicScore(int[,] grid, int row, int col)
+    {
+        int height = grid[row, col];
+
+        // Looking left
+        int leftScore = 0;
+        for (int c = col - 1; c >= 0; c--)
+        {
+            leftScore++;
+            if (grid[row, c] >= height) {  break; }
+        }
+
+        // Looking right
+        int rightScore = 0;
+        for (int c = col + 1; c < grid.GetLength(1); c++)
+        {
+            rightScore++;
+            if (grid[row, c] >= height) { break; }
+        }
+
+        // Looking up
+        int upScore = 0;
+        for (int r = row - 1; r >= 0; r--)
+        {
+            upScore++;
+            if (grid[r, col] >= height) { break; }
+        }
+
+        // Looking down
+        int downScore = 0;
+        for (int r = row + 1; r < grid.GetLength(0); r++)
+        {
+            downScore++;
+            if (grid[r, col] >= height) { break; }
+        }
+
+        return leftScore * rightScore * upScore * downScore;
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        int[,] grid = ParseGrid();
+
+        int totalVisible = 0;
+        for (int row = 0; row < grid.GetLength(0); row++)
+        {
+            for (int col = 0; col < grid.GetLength(1); col++)
+            {
+                if (IsVisible(grid, row, col)) totalVisible++;
+            }
+        }
+
+        return totalVisible.ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        int[,] grid = ParseGrid();
+
+        int bestScenicScore = 0;
+        for(int row = 0; row < grid.GetLength(0); row++)
+        {
+            for (int col = 0; col < grid.GetLength(1); col++)
+            {
+                int scenicScore = ScenicScore(grid, row, col);
+                if (scenicScore > bestScenicScore) bestScenicScore = scenicScore;
+            }
+        }
+
+        return bestScenicScore.ToString();
+    }
+
+    private int[,] ParseGrid()
+    {
+        var splitLines = this.Input!.SplitByNewline();
+        int[,] grid = new int[splitLines.Length, splitLines[0].Length];
+
+        for (int row = 0; row < splitLines.Length; row++)
+        {
+            string? line = splitLines[row];
+            for (int col = 0; col < line.Length; col++)
+            {
+                int height = int.Parse(line[col].ToString());
+                grid[row, col] = height;
+            }
+        }
+
+        return grid;
     }
 }
--- AdventOfCode.Solutions/Year2022/Day09/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day09/Solution.cs
@@ -2,15 +2,121 @@ namespace AdventOfCode.Solutions.Year2022.Day09;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(09, 2022, "") { }
+    public Solution() : base(09, 2022, "Rope Bridge") { }
+
+    static (int x, int y) MoveHead((int x, int y) headPosition, char direction)
+    {
+        switch (direction)
+        {
+            case 'L':
+                headPosition.x--;
+                break;
+            case 'R':
+                headPosition.x++;
+                break;
+            case 'D':
+                headPosition.y--;
+                break;
+            case 'U':
+                headPosition.y++;
+                break;
+            default:
+                break;
+        }
+
+        return headPosition;
+    }
+
+    static (int x, int y) MoveTail((int x, int y) followPosition, (int x, int y) tailPosition)
+    {
+        var maxDistance = 1;
+
+        if (followPosition.x < tailPosition.x - maxDistance)
+        {
+            tailPosition.x--;
+            if (followPosition.y > tailPosition.y) { tailPosition.y++; }
+            if (followPosition.y < tailPosition.y) { tailPosition.y--; }
+        }
+        else if (followPosition.x > tailPosition.x + maxDistance)
+        {
+            tailPosition.x++;
+            if (followPosition.y > tailPosition.y) { tailPosition.y++; }
+            if (followPosition.y < tailPosition.y) { tailPosition.y--; }
+        }
+        else if (followPosition.y < tailPosition.y - maxDistance)
+        {
+            tailPosition.y--;
+            if (followPosition.x > tailPosition.x) { tailPosition.x++; }
+            if (followPosition.x < tailPosition.x) { tailPosition.x--; }
+        }
+        else if (followPosition.y > tailPosition.y + maxDistance)
+        {
+            tailPosition.y++;
+            if (followPosition.x > tailPosition.x) { tailPosition.x++; }
+            if (followPosition.x < tailPosition.x) { tailPosition.x--; }
+        }
+
+        return tailPosition;
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var visitedPositions = new HashSet<(int, int)>();
+        var headPosition = (x:0, y:0);
+        var tailPosition = (x:0, y:0);
+        visitedPositions.Add(tailPosition);
+
+        string[] lines = this.Input.SplitByNewline();
+        for (int lineIndex = 0; lineIndex < lines.Length; lineIndex++)
+        {
+            string? line = lines[lineIndex];
+            char direction = line[0];
+            int steps = int.Parse(line[2..]);
+
+            for (int i = 0; i < steps; i++)
+            {
+                headPosition = MoveHead(headPosition, direction);
+
+                tailPosition = MoveTail(headPosition, tailPosition);
+
+                visitedPositions.Add(tailPosition);
+            }
+        }
+
+        return visitedPositions.Count.ToString();     
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        var visitedPositions = new HashSet<(int, int)>();
+        var headPosition = (x: 0, y: 0);
+        var tailPositions = new (int x, int y)[9];
+        
+        visitedPositions.Add(tailPositions[^1]);
+
+        string[] lines = this.Input.SplitByNewline();
+        for (int lineIndex = 0; lineIndex < lines.Length; lineIndex++)
+        {
+            string? line = lines[lineIndex];
+            char direction = line[0];
+            int steps = int.Parse(line[2..]);
+
+            for (int i = 0; i < steps; i++)
+            {
+                headPosition = MoveHead(headPosition, direction);
+
+                for (int j = 0; j < tailPositions.Length; j++)
+                {
+                    tailPositions[j] = MoveTail(
+                        j == 0 ? headPosition: tailPositions[j - 1], 
+                        tailPositions[j]
+                    );
+                }
+
+                visitedPositions.Add(tailPositions[^1]);
+            }
+        }
+
+        return visitedPositions.Count.ToString();
     }
 }
